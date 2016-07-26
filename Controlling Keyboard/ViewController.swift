3a3bc37418eb263c99c3a79e96cf4dba853e3747
Bikram Dangol <bikramdangol@hotmail.com>
2016-07-25 18:40:45 -0600

    Example to control keyboard

--- Controlling Keyboard/ViewController.swift
+++ Controlling Keyboard/ViewController.swift
@@ -8,7 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    // It is called when you hit return in the keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
+    // It is called when you touch outside of the text field.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +28,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
